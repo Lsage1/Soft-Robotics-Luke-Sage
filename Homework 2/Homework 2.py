8b56fcc841ba11224b42985cbbf690c0a7d276eb
Redprint0 <106635915+Redprint0@users.noreply.github.com>
2025-10-22 17:01:19 -0700

    HW 2 Task 1 Largely complete. Project plots simply supported beam and position of lowest node over time.
    Additional Formatting for homework 1 pdf document

--- Homework 2/Homework 2.py
+++ Homework 2/Homework 2.py
@@ -8,7 +8,7 @@ from gradEb import gradEb
 from hessEs import hessEs
 
 
-nv = 11 # number of nodes/vertices
+nv = 50 # number of nodes/vertices
 ndof = 2 * nv
 midNode = nv//2 + 1
 
@@ -16,10 +16,10 @@ midNode = nv//2 + 1
 dt = 0.01 # second
 
 # Rod length
-RodLength = 1 # meter
+rodLength = 1 # meter
 
 # Discrete length / reference length
-deltaL = RodLength / (nv - 1)
+deltaL = rodLength / (nv - 1)
 
 # Radii of spheres (given)
 R = np.zeros(nv)
@@ -32,11 +32,12 @@ rho_metal = 7000 # kg/m^3
 rho_gl = 1000
 rho = rho_metal  - rho_gl
 
-# Cross-sectional radius
-r0 = 1e-3 # meter
+# Cross-sectional radii
+innerRadius = 0.011 #m
+outerRadius = 0.013 #m
 
 # Young's modulus
-Y = 1e9
+ymod = 7e10 # Pa
 
 # Viscosity
 visc = 1000.0 # Pa-s
@@ -45,21 +46,21 @@ visc = 1000.0 # Pa-s
 maximum_iter = 1000
 
 # Total time
-totalTime = 50 # second
+totalTime = 1 # second
 
 # Variables related to plotting
 saveImage = 0
-plotStep = 250 # Every 5-th step will be plotted
+plotStep = 20 # Every 5-th step will be plotted
 
 ######################################################################################################
 
 # Utility quantites
 ne = nv - 1 # number of edges
-EI = Y * np.pi * r0**4 / 4
-EA = Y * np.pi * r0**2
+EI = ymod * np.pi * (outerRadius**4 - innerRadius**4) / 4 # Flexural Rigidity = Moment of Interia * Young's Modulus
+EA = ymod * np.pi * (outerRadius**2 - innerRadius**2) # Axial Rigidity N/m^3
 
 # Tolerance
-tol = EI / RodLength ** 2 * 1e-3
+tol = EI / rodLength ** 2 * 1e-3
 
 # Geometry
 nodes = np.zeros((nv, 2))
@@ -68,25 +69,37 @@ for c in range(nv):
   nodes[c, 1] = 0.0 # y-coordinate
 
 # Mass vector and matrix
+density = 2700 # kg/m^3
+nodeMass = np.pi * (outerRadius**2 - innerRadius**2) * rodLength * density / (nv - 1)
+
 m = np.zeros( 2 * nv )
 for k in range(0, nv):
-  m[2*k] = 4/3 * np.pi * R[k]**3 * rho_metal # mass of k-th node along x
-  m[2*k + 1] = 4/3 * np.pi * R[k]**3 * rho_metal # mass of k-th node along y
+  m[2*k] = nodeMass # mass of k-th node along x
+  m[2*k + 1] = nodeMass # mass of k-th node along y
 mMat = np.diag(m)
 
 # Gravity (external force)
-W = np.zeros( 2 * nv)
-g = np.array([0, -9.8]) # m/s^2
-for k in range(0, nv):
-  W[2*k] = 4.0 / 3.0 * np.pi * R[k]**3 * rho * g[0] # Weight along x
-  W[2*k+1] = 4.0 / 3.0 * np.pi * R[k]**3 * rho * g[1] # Weight along y
+W = np.zeros( 2 * nv) # Weight is set to zero
+P = -2000
+loadPos = 0.75 # x position at which to apply load approximately
+
+loadIdx = round(nv * loadPos/rodLength) - 1 # Get the index of the node closest to the load position
+#print(loadIdx)
+W[loadIdx * 2 + 1] = P # Assign load to the y component of the node at the load position
+#print(W)
+
+#g = np.array([0, -9.8]) # m/s^2
+#for k in range(0, nv):
+#  W[2*k] = 4.0 / 3.0 * np.pi * R[k]**3 * rho * g[0] # Weight along x
+#  W[2*k+1] = 4.0 / 3.0 * np.pi * R[k]**3 * rho * g[1] # Weight along y
 # Gradient of W = 0
 
+
 # Viscous damping (external force)
-C = np.zeros((2 * nv, 2 * nv))
-for k in range(0, nv):
-  C[2*k, 2*k] = 6.0 * np.pi * visc * R[k] # Damping along x for k-th node
-  C[2*k+1, 2*k+1] = 6.0 * np.pi * visc * R[k] # Damping along y for k-th node
+C = np.zeros((2 * nv, 2 * nv)) # Viscous Damping is set to zero
+#for k in range(0, nv):
+#  C[2*k, 2*k] = 6.0 * np.pi * visc * R[k] # Damping along x for k-th node
+#  C[2*k+1, 2*k+1] = 6.0 * np.pi * visc * R[k] # Damping along y for k-th node
 
 # Initial conditions
 q0 = np.zeros(2 * nv)
@@ -100,7 +113,8 @@ u0 = np.zeros(2 * nv) # old velocity
 # Boundary Conditions:
 
 all_DOFs = np.arange(ndof) # Set of all DOFs
-fixed_index = np.array([0, 1, 2, 3]) # Fixed DOFs
+fixed_index = np.array([0, 1, nv*2-1]) # Fixed DOFs
+#print(all_DOFs)
 
 # Free index
 free_index = np.setdiff1d(all_DOFs, fixed_index) # All the DOFs are free except the fixed ones
@@ -112,14 +126,12 @@ Nsteps = round( totalTime / dt )
 
 ctime = 0 # Current time
 
-# Store the y-coordinate of the middle node, its velocity, and the angle
+# Store the y-coordinate of the bottom node
 all_pos = np.zeros(Nsteps)
-all_vel = np.zeros(Nsteps)
-mid_angle = np.zeros(Nsteps)
+
 
 all_pos[0] = 0
-all_vel[0] = 0
-mid_angle[0] = 0
+
 
 # Loop over the time steps
 for timeStep in range(1,Nsteps):
@@ -135,12 +147,12 @@ for timeStep in range(1,Nsteps):
 
   ctime += dt # Update current time
 
-  # Save information about the middle node
-  all_pos[timeStep] = q_new[2*midNode-1] # y coordinate of middle node
-  all_vel[timeStep] = u_new[2*midNode-1] # y velocity of middle node
-  vec1 = np.array( [ q_new[2*midNode-2], q_new[2*midNode-1], 0 ] ) - np.array( [ q_new[2*midNode-4], q_new[2*midNode-3], 0 ] ) # Second node - First node
-  vec2 = np.array( [ q_new[2*midNode], q_new[2*midNode+1], 0 ] ) - np.array( [ q_new[2*midNode-2], q_new[2*midNode-1], 0 ] ) # Third node - Second node
-  mid_angle[timeStep] =  np.degrees( np.arctan2(np.linalg.norm( np.cross(vec1, vec2)), np.dot(vec1, vec2)) )
+  # Save position of the bottom node
+  minY = (min(q_new[1::2])) # Get the minimum y component of position at this time step
+
+  all_pos[timeStep] = minY # y coordinate of bottom node
+
+
 
   q0 = q_new.copy() # New position becomes old position
   u0 = u_new.copy() # New velocity becomes old velocity
@@ -169,26 +181,14 @@ t_arr = np.linspace(0, totalTime, Nsteps)
 plt.figure(2)
 plt.plot(t_arr, all_pos, 'ko-')
 plt.xlabel('Time (s)')
-plt.ylabel('Middle Node Position (m)')
-plt.title('Middle Node Position vs. Time')
+plt.ylabel('Bottom Node Position (m)')
+plt.title('Bottom Node Position vs. Time')
 plt.show()
 if saveImage:
-    plt.savefig('middle_node_position.png')
+    plt.savefig('bottom_node_position.png')
 
-plt.figure(3)
-plt.plot(t_arr, all_vel, 'ko-')
-plt.xlabel('Time (s)')
-plt.ylabel('Middle Node Velocity (m/s)')
-plt.title('Middle Node Velocity vs. Time')
-plt.show()
-if saveImage:
-    plt.savefig('middle_node_velocity.png')
 
-plt.figure(4)
-plt.plot(t_arr, mid_angle, 'ko-')
-plt.xlabel('Time (s)')
-plt.ylabel('Turning angle (degrees)')
-plt.title('Turning angle vs. Time')
-plt.show()
-if saveImage:
-    plt.savefig('turning_angle.png')
+########################################################################
+# Plot theoretical euler beam theory
+
+#ymax_T = P * c
