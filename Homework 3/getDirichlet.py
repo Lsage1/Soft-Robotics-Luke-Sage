8f673dc0e1f6b2bbb8dcb326819cb86b1091c07e
Redprint0 <106635915+Redprint0@users.noreply.github.com>
2025-11-01 16:34:15 -0700

    Save in case laptop runs out of power

--- Homework 3/getDirichlet.py
+++ Homework 3/getDirichlet.py
@@ -1,14 +1,15 @@
 
 
-def getDirichlet(x_mid, y_mid, q_guess, tol, maximum_iter, EI, EA, W, deltaL, free_index):
+def getDirichlet(x_mid, y_mid, nv, q_guess, tol, maximum_iter, EI, EA, W, deltaL, free_index):
 
     dx = 10e-2 * deltaL # X perturbation value
     dy = 10e-2 * deltaL # y perturbation value
     dtheta = 10e-3 # Theta perturbation value
 
-    q_predicted, error = staticSolver(q_guess, tol, maximum_iter, EI, EA, W, deltaL,free_index)
+    q_original, error = staticSolver(q_guess, tol, maximum_iter, EI, EA, W, deltaL,free_index)
     if error < 0:
         print('Static Solver could not converge.')
 
+    x_original = q_original[(nv+1)/2] # Get middle position
 
     return xc, yc, thetac, error
