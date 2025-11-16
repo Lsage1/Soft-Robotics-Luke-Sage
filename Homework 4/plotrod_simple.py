3d5b29b8014386971ac16d50f24e31ddf22abe01
Redprint0 <106635915+Redprint0@users.noreply.github.com>
2025-11-15 16:16:40 -0800

    Homework 4 - All Functions and main set up properly.

---
+++ Homework 4/plotrod_simple.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from set_axes_equal import set_axes_equal
+
+def plotrod_simple(q, ctime):
+    """
+    Function to plot the rod with the position and directors.
+
+    Parameters:
+    - q: Position vector (DOF vector).
+    - ctime: Current time for title.
+    """
+
+    x1 = q[0::4]
+    x2 = q[1::4]
+    x3 = q[2::4]
+
+    fig = plt.figure(1)
+    plt.clf()  # Clear the figure
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the rod as black circles connected by lines
+    ax.plot3D(x1, x2, x3, 'ko-')
+
+    # Plot the first node with a red triangle
+    ax.plot3D([x1[0]], [x2[0]], [x3[0]], 'r^')
+
+    # Set the title with current time
+    ax.set_title(f't={ctime:.2f}')
+
+    # Set axes labels
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    # Set equal scaling and a 3D view
+    set_axes_equal(ax)
+    plt.draw()  # Force a redraw of the figure
+
+    plt.show()
