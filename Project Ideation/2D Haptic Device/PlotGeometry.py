80df8e710c79b3df6349dc31edeb72a4feb56dd7
lukea <lasage@ucla.edu>
2025-11-25 23:55:29 -0800

    Discrete Rod Structure running, buggy.

--- Project Ideation/2D Haptic Device/PlotGeometry.py
+++ Project Ideation/2D Haptic Device/PlotGeometry.py
@@ -1,17 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def PlotGeometry(vertices, edgeIndex, junctions=None):
+def PlotGeometry(q0, edgeIndex):
     """
     Plot vertices and edges of a discrete rod network.
 
     Parameters:
-        vertices (np.ndarray): Array of vertex coordinates, shape (N, 2)
+        q0 (np.ndarray): Linearized vertex coordinates, shape (2*N,)
         edgeIndex (np.ndarray): Array of edges, shape (M, 2)
-        junctions (list or np.ndarray, optional): Indices of junction vertices to highlight
     """
-    plt.figure(figsize=(6, 6))
+    # Reshape q0 to (N, 2) for plotting
+    vertices = q0.reshape(-1, 2)
+
+
 
     # Plot edges
     for edge in edgeIndex:
@@ -21,16 +22,7 @@ def PlotGeometry(vertices, edgeIndex, junctions=None):
     # Plot vertices
     plt.scatter(vertices[:, 0], vertices[:, 1], color='red', s=50, zorder=5)
 
-    # Highlight junctions if provided
-    if junctions is not None:
-        plt.scatter(vertices[junctions, 0], vertices[junctions, 1], color='green', s=100, label='Junctions', zorder=6)
-
     plt.axis('equal')
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Discrete Rod Network')
-    if junctions is not None:
-        plt.legend()
-    plt.show()
-
-
